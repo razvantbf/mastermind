55ebb11b77bc88f0986cd72b7f8d9886e7a485a3
Razvan Cazanescu <razvan@thebasicfundamentals.com>
2015-07-11 12:21:16 +0200

    made the transition perfect from old version from 5am.ro template, to stand alone website

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ var gulp = require('gulp'),
     prefix = require('gulp-autoprefixer'),
     connect = require('gulp-connect'),
     imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant');
+    pngquant = require('imagemin-pngquant'),
+    pixrem = require('gulp-pixrem');
 // }}}
 // COMPONENTS SOURCES {{{
 jsSources = ['components/scripts/*.js'];
@@ -45,6 +46,7 @@ gulp.task('sass', function() {
     gulp.src(sassMain)
         .pipe(sass({style: 'expanded'}).on('error', gutil.log))
         .pipe(prefix("last 3 versions", "ie 8", "ie 9"))
+        .pipe(pixrem('10px'))
         //.pipe(minify())
         .pipe(gulp.dest('build/css'))
         .pipe(connect.reload());
